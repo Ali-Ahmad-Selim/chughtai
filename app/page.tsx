0a75d654adff7e88e00e392c845794b21ba996b4
Ali ahmad <ali732152ahmad@gmail.com>
2025-11-25 10:50:21 +0500

    link

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { QRCodeCanvas } from "qrcode.react";
 const page = () => {
-   const link = "/awais.pdf"; 
+   const link = "https://chughtai-ruddy.vercel.app/awais.pdf"; 
   return (
     <div>
  <a href={link} target="_blank" rel="noopener noreferrer">
