afacdbfe970c259329fbffcbd46a79554ecb6e72
Ali ahmad <ali732152ahmad@gmail.com>
2025-11-25 10:52:52 +0500

    center

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { QRCodeCanvas } from "qrcode.react";
 const page = () => {
    const link = "https://chughtai-ruddy.vercel.app/awais.pdf"; 
   return (
-    <div>
+    <div className='h-[100px] bg-white mx-auto flex justify-center items-center'>
  <a href={link} target="_blank" rel="noopener noreferrer">
       <QRCodeCanvas value={link} size={180} />
     </a>    </div>
