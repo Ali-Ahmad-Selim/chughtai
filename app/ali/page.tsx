bb53eced6c033284270912c5e6bfb5dd3a79ffc0
Ali ahmad <ali732152ahmad@gmail.com>
2025-12-09 06:33:48 +0500

    ali

--- app/ali/page.tsx
+++ app/ali/page.tsx
@@ -1,10 +1,13 @@
 
 
+
+
+
 "use client"
 import React from 'react'
 import { QRCodeCanvas } from "qrcode.react";
 const page = () => {
-   const link = "https://chughtai-ruddy.vercel.app/Muneeb.pdf"; 
+   const link = "https://chughtai-ruddy.vercel.app/aliahmad.pdf"; 
   return (
    <div className="h-screen w-screen bg-white flex justify-center items-center">
   <a href={link} target="_blank" rel="noopener noreferrer">
